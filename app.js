e67b4f006a9cefd03a91a6d511f23cb3e08862e2
barcodech <103811940+barcodech@users.noreply.github.com>
2022-06-06 01:44:38 +0700

    Add files via upload

---
+++ app.js
@@ -0,0 +1,16 @@
+var num = 100;
+var firut1 = "durian";
+var fruit2 = "mango";
+var TrueOrFalse = false;
+var noValue = null;
+
+console.log("My number is",num);
+console.log("My fruit is",firut1+" and "+fruit2);
+console.log("True or False:",TrueOrFalse)
+console.log(noValue)
+
+let myVariable =100;
+
+const pi = 3.141;
+
+console.log(pi);
