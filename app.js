84540741e7cc418c31d54e26b61d35be9ee485b2
barcodech <103811940+barcodech@users.noreply.github.com>
2022-06-06 02:38:50 +0700

    Add files via upload

---
+++ app.js
@@ -0,0 +1,8 @@
+// spread operator = ...
+/*
+let numbers = [1,2,3,4,5,6,7,8,9,10]; 
+let maximum = Math.max(... numbers) 
+console.log(maximum); 
+*/
+let foods = ["pizza", "padthai", "roti"]; 
+console.log(...foods);
