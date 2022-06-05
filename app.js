322af404df0f5d36d77ea6a304546edc9281aa8a
barcodech <103811940+barcodech@users.noreply.github.com>
2022-06-06 02:04:34 +0700

    Add files via upload

---
+++ app.js
@@ -0,0 +1,13 @@
+//let letters = ["A","B","C","D"];
+
+let phone = {
+    price: 50000,
+    model: "iphone13",
+    year: 2022,
+    color: "white"
+}
+
+for(let property in phone){
+
+    console.log(phone[property]);
+}
