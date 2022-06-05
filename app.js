481f54a5a2906f70db9110b8167069814f55fff7
barcodech <103811940+barcodech@users.noreply.github.com>
2022-06-06 02:06:04 +0700

    Add files via upload

---
+++ app.js
@@ -0,0 +1,21 @@
+
+//var num;
+
+//num = Math.round(3.4);
+//num = Math.floor(3.9);
+//num = Math.ceil(3.2);
+//num = Math.pow(2,2);
+//num = Math.sqrt(16);
+//num = Math.abs(-20);
+//num = Math.min(1,20,52,30);
+//num = Math.max(1,20,52,30);
+//num = Math.PI;
+//num = Math.E;
+
+var a = window.prompt("Enter side A");
+var b = window.prompt("Enter side B");
+var c;
+
+c = Math.sqrt(Math.pow(a,2) + Math.pow(b,2))
+
+console.log(c);
