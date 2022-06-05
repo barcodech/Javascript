1e03be24a5ef2e66cf8d13274a7ebc2929c33232
barcodech <103811940+barcodech@users.noreply.github.com>
2022-06-06 01:52:05 +0700

    Add files via upload

---
+++ app.js
@@ -0,0 +1,18 @@
+var locker = ["book","phone","keys"]
+
+//locker.sort();
+//locker.reverse();
+//console.log(locker);
+
+console.log(locker[0]);
+console.log(locker[1]);
+console.log(locker[2]);
+var lastlocker = locker[locker.length-1];
+console.log(lastlocker);
+
+locker.push("hats");
+locker.pop();
+console.log(locker);
+
+var numberOflocker = locker.length;
+console.log(numberOflocker);
