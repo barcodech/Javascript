1701b18a4cb6c5202bbb68429b829dacbafd3a1a
barcodech <103811940+barcodech@users.noreply.github.com>
2022-06-06 01:53:34 +0700

    Add files via upload

---
+++ app.js
@@ -0,0 +1,28 @@
+var points = 75;
+
+if(points >= 80){
+    console.log("You get grade 4");
+}
+else if(points >=70){
+    console.log("You get grade 3");
+}
+else if(points >=60){
+    console.log("You get grade 2");
+}
+else if(points >=50){
+    console.log("You get grade 1");
+}
+else{
+    console.log("You get grade 0");
+}
+
+*/
+
+var pass = true;
+
+if(pass){
+    console.log("You are passed")
+}
+else{
+    console.log("You are failed")
+}
