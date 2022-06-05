0dd6fc25d4ee8c41547dc59a54d84fb221f59a53
barcodech <103811940+barcodech@users.noreply.github.com>
2022-06-06 01:48:49 +0700

    Add files via upload

---
+++ app.js
@@ -0,0 +1,18 @@
+var num = window.prompt("What number do you want?");
+
+num = Number(num);100
+num += 5;
+
+console.log(typeof num)
+console.log(num)
+
+/*
+var = num;
+
+num = Number(10);
+num = String("ok");
+num = Boolean(1);
+
+console.log(typeof num)
+console.log(num)
+*/
