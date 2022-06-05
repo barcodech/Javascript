b54e05d0480455502af6e3e3c64a4a3fe50b6f5d
barcodech <103811940+barcodech@users.noreply.github.com>
2022-06-06 01:45:34 +0700

    Add files via upload

---
+++ app.js
@@ -0,0 +1,29 @@
+var coconut = 10;
+
+//coconut = coconut + 1;
+//coconut = coconut - 1;
+//coconut = coconut * 2;
+//coconut = coconut / 2;
+//coconut = coconut % 3;
+//coconut = coconut **2;
+
+//coconut +=1;
+//coconut -=1;
+//coconut *=2;
+//coconut /=2;
+//coconut %=3;
+//coconut **=3;
+
+/* 
+    ลำดับการคิดเลข
+    1. วงเล็บ ()
+    2. ยกกำลัง
+    3. คูณ * & หาร /
+    4. บวก + & ลบ -
+*/
+
+coconut = 20 + 5 * 2;
+coconut = (10+2)/3;
+
+
+console.log(coconut)
