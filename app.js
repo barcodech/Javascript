e37da0c6165b39540f7d9c91b4d456844540c73b
barcodech <103811940+barcodech@users.noreply.github.com>
2022-06-06 02:13:25 +0700

    Add files via upload

---
+++ app.js
@@ -0,0 +1,14 @@
+let username = "Oceania code";
+
+//username = username.length
+//username = username.charAt(11);
+//username = username.indexOf("i");
+//username = username.lastIndexOf("a");
+//username = username.trim();
+//username = username.toLocaleUpperCase();
+//username = username.toLocaleLowerCase();
+username = username.replaceAll("a","z");
+
+
+
+console.log(username);
