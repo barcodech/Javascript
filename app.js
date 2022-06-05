513ee642d7194d446143a94559afb71be5bfa843
barcodech <103811940+barcodech@users.noreply.github.com>
2022-06-06 01:57:53 +0700

    Add files via upload

---
+++ app.js
@@ -0,0 +1,11 @@
+//  loose equality operator ==
+//  strict equality operator ===
+
+var string = "10";
+
+if (string === "10"){
+    console.log("Yes, this is string")
+}
+else{
+    console.log("Yes, this is not string")
+}
