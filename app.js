534428aeb99faa76277d318bb630661ad95a9d0e
barcodech <103811940+barcodech@users.noreply.github.com>
2022-06-06 01:46:43 +0700

    Add files via upload

---
+++ app.js
@@ -0,0 +1,10 @@
+//var place = window.prompt("Where do you live?");
+
+//console.log("I live in:",place)
+
+document.getElementById("button").onclick = function(){
+
+    var myPlace = document.getElementById("text").value;
+    console.log("I live in:",myPlace)
+
+}
