d396befcf9fd2fbcf7e4157214b4e039a8259894
barcodech <103811940+barcodech@users.noreply.github.com>
2022-06-06 03:04:03 +0700

    Add files via upload

---
+++ app.js
@@ -0,0 +1,39 @@
+const bike1 = {
+    model:"honda",
+    color:"white",
+    year:2009,
+  
+    drive : function(){
+      console.log("You are riding");
+    },
+    brake : function(){
+      console.log("You are braking")
+    }
+  }
+  
+  const bike2 = {
+    model:"yamaha",
+    color:"black",
+    year:2010,
+  
+    drive : function(){
+      console.log("You are riding");
+    },
+    brake : function(){
+      console.log("You are braking")
+    }
+  }
+  
+  console.log(bike1.model);
+  console.log(bike1.color);
+  console.log(bike1.year);
+  
+  bike1.drive();
+  bike1.brake();
+  
+  console.log(bike2.model);
+  console.log(bike2.color);
+  console.log(bike2.year);
+  
+  bike2.drive();
+  bike2.brake();
