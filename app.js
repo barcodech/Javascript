dc8686cb2420c57bfa0b0eaaf79b0b31566bd5da
barcodech <103811940+barcodech@users.noreply.github.com>
2022-06-06 07:54:28 +0700

    Add files via upload

---
+++ app.js
@@ -0,0 +1,17 @@
+const innerDiv = document.getElementById("innerDiv");
+const outerDiv = document.getElementById("outerDiv");
+ 
+innerDiv.addEventListener("click", changeBlue);
+outerDiv.addEventListener("click", changeBlue, true);
+ 
+function changeBlue(){
+    alert(`You selected ${this.id}`);
+    this.style.backgroundColor = "lightblue"
+}
+ 
+innerDiv.addEventListener("mouseover", changeBlue);
+innerDiv.addEventListener("mouseout", changeGreen);
+ 
+function changeBlue(){
+    innerDiv.style.backgroundColor = "blue";
+}
